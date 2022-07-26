5b0a486ea4f1563fbb43b679fbf215d01202a095
vishnuacappella <acappella.studiov@gmail.com>
2022-07-26 16:30:52 +0530

    higher order function

--- bin/dart_get_up_to_speed.dart
+++ bin/dart_get_up_to_speed.dart
@@ -1,47 +1,14 @@
-void main(List<String> arguments) {}
-
-// * Positional Parameters
-
-void positionalParameters(int x, double y, String greeting) {
-  positionalParameters(3, 5.5, 'hi');
+void main(List<String> arguments) {
+  final twicePlusFive = twice((x) => x + 5);
+  final result = twicePlusFive(3);
+  print(result);
 }
 
-// * Optional Positional Parameters
+// * Function return another function
+typedef IntTransformer = int Function(int);
 
-void optionalPositionalParameters(int x, double y, [String? greeting]) {
-  optionalPositionalParameters(3, 5.5, 'optional');
-}
-
-void optionalPositionalParameters1(int x, double y, [String greeting = 'hi']) {
-  optionalPositionalParameters1(3, 5.5);
-}
-
-// * Named Optional Parameters
-void namedOptionalParameters({
-  int? x,
-  double? y,
-  String? greeting, //! greeting ='hi'
-}) {
-  namedOptionalParameters(x: 5, greeting: 'hi');
-}
-
-// * Named Required Parameter
-void namedRequiredParameters({
-  required int x,
-  required double y,
-  required String greeting,
-}) {
-  namedRequiredParameters(x: 2, y: 2.5, greeting: 'greeting');
-}
-
-// * Combine Named Required Parameter with Positional parameter
-
-void combineNamedRequiredToPositionalParameters(
-  int positional, {
-  required int x,
-  required double y,
-  required String greeting,
-}) {
-  combineNamedRequiredToPositionalParameters(10,
-      x: 2, y: 2.5, greeting: 'greeting');
+IntTransformer twice(IntTransformer f) {
+  return (int x) {
+    return f(f(x));
+  };
 }
