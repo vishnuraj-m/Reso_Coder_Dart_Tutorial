547afeefcf4d76fd24e1aa8f2516c6a5e6c93fb0
vishnuacappella <acappella.studiov@gmail.com>
2022-07-28 06:12:06 +0530

    Classes Properties

--- bin/dart_get_up_to_speed.dart
+++ bin/dart_get_up_to_speed.dart
@@ -1,24 +1,33 @@
 void main(List<String> arguments) {
-  final x = Example(1, 2);
-  x._private;
+  final user = User(
+    firstName: 'Vishnu',
+    secondName: 'Ram',
+    email: 'v@r.com',
+  );
+  print(user.email);
 }
 
-class Example {
-  int public;
-  int _private;
+class User {
+  final String firstName;
+  final String secondName;
+  String? _email;
 
-  Example(this.public, this._private);
-
-  Example.namedConstructor({
-    required this.public,
-    required int privateParameter,
-  }) : _private = privateParameter;
-
-  void myMethod() {
-    _private;
+  User({
+    required this.firstName,
+    required this.secondName,
+    required String email,
+  }) {
+    this.email = email;
   }
-}
 
-class NonInstantiable {
-  NonInstantiable._();
+  String get fullName => '$firstName $secondName';
+  String get email => _email ?? 'Email not present';
+
+  set email(String value) {
+    if (value.contains('@')) {
+      _email = value;
+    } else {
+      _email = null;
+    }
+  }
 }
--- bin/new_file.dart
+++
@@ -1,7 +0,0 @@
-import 'dart_get_up_to_speed.dart';
-
-void exampleMethod() {
-  final x = Example(1, 2);
-  x._private;
-  NonInstantiable._();
-}
