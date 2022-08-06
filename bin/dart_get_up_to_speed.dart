5d3e5f42d1c1fff30abcee48c8896dd35b2a8ec6
vishnuacappella <acappella.studiov@gmail.com>
2022-08-06 12:34:19 +0530

    Immutable Data Classes Basic

--- bin/dart_get_up_to_speed.dart
+++ bin/dart_get_up_to_speed.dart
@@ -1,10 +1,48 @@
-part 'other_files.dart';
+import 'dart:convert';
+
+import 'package:meta/meta.dart';
 
 void main(List<String> arguments) {
-  _oppositeWay();
+  final person1 = Person(name: 'Vishnu', age: 20);
+  final person1Updated = person1.copyWith(age: person1.age + 1);
 }
 
-class _PackagePrivateClass {
-  final String _packagePrivateField;
-  _PackagePrivateClass._(this._packagePrivateField);
+@immutable
+class Person {
+  final String name;
+  final int age;
+  Person({
+    required this.name,
+    required this.age,
+  });
+
+  Person copyWith({
+    String? name,
+    int? age,
+  }) {
+    return Person(
+      name: name ?? this.name,
+      age: age ?? this.age,
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'name': name,
+      'age': age,
+    };
+  }
+
+  @override
+  String toString() => 'Person(name: $name, age: $age)';
+
+  @override
+  bool operator ==(Object other) {
+    if (identical(this, other)) return true;
+
+    return other is Person && other.name == name && other.age == age;
+  }
+
+  @override
+  int get hashCode => name.hashCode ^ age.hashCode;
 }
--- bin/other_files.dart
+++
@@ -1,9 +0,0 @@
-part of 'dart_get_up_to_speed.dart';
-
-void example() {
-  _PackagePrivateClass._('Hello');
-}
-
-void _oppositeWay() {
-  print('Hello');
-}
