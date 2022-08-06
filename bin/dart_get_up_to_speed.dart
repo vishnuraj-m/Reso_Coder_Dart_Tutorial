ebaaf8645cbc990023440bf04ca69bba059b0eff
vishnuacappella <acappella.studiov@gmail.com>
2022-08-06 18:02:09 +0530

    Freezed Data Classes

--- bin/dart_get_up_to_speed.dart
+++ bin/dart_get_up_to_speed.dart
@@ -1,48 +1,16 @@
-import 'dart:convert';
-
-import 'package:meta/meta.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+part 'dart_get_up_to_speed.freezed.dart';
 
 void main(List<String> arguments) {
   final person1 = Person(name: 'Vishnu', age: 20);
   final person1Updated = person1.copyWith(age: person1.age + 1);
 }
 
-@immutable
-class Person {
-  final String name;
-  final int age;
-  Person({
-    required this.name,
-    required this.age,
-  });
-
-  Person copyWith({
-    String? name,
-    int? age,
-  }) {
-    return Person(
-      name: name ?? this.name,
-      age: age ?? this.age,
-    );
-  }
-
-  Map<String, dynamic> toMap() {
-    return {
-      'name': name,
-      'age': age,
-    };
-  }
-
-  @override
-  String toString() => 'Person(name: $name, age: $age)';
-
-  @override
-  bool operator ==(Object other) {
-    if (identical(this, other)) return true;
-
-    return other is Person && other.name == name && other.age == age;
-  }
-
-  @override
-  int get hashCode => name.hashCode ^ age.hashCode;
+@freezed
+class Person with _$Person {
+  const Person._();
+  const factory Person({
+    required String name,
+    required int age,
+  }) = _Person;
 }
